1faca26223628e5e1dcfff2f4cd35807bc2b53b7
makeMHDB <57128981+makeMHDB@users.noreply.github.com>
2019-11-28 12:48:05 +0200

    2. Mėnesių pavadinimai

--- js/data.js
+++ js/data.js
@@ -13,4 +13,4 @@ const account = [
     { month: 12, expense: 200 }
 ];
 
-const months = ['Sausis', 'Vasaris', 'Kovas'];
+const months = ['Sausis', 'Vasaris', 'Kovas', 'Balandis', 'Gegužė', 'Birželis', 'Liepa', 'Rugpjūtis', 'Rugsėjis', 'Spalis', 'Lapkritis', 'Gruodis'];
